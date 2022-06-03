8318a8d0b77ffe53639c769a0e45824478699a6f
Maysummer <udojyk@gmail.com>
2022-06-02 19:10:52 -0700

    print all the names defined by a compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from hidden_4 import *
+import hidden_4
 names = dir(hidden_4)
 if __name__ == "__main__":
     for i in names:
-        print("{}".format(i))
+        if i[0] != "_":
+            print("{}".format(i))

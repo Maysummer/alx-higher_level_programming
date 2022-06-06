1c15d69d6ead65640bdb70294895d872623ceb27
Maysummer <udojyk@gmail.com>
2022-06-06 09:30:07 -0700

    max element in list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_num = my_list[0]
     if len(my_list) == 0:
         return
+    max_num = my_list[0]
     for i in my_list:
         if i > max_num:
             max_num = i

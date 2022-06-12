23cacb32c332548922a4f59af28d802171af392c
Maysummer <udojyk@gmail.com>
2022-06-12 05:28:47 -0700

    returns a key with the biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    order = sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)
-    return order[0][0]
+    if a_dictionary:
+        order = sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)
+        return order[0][0]

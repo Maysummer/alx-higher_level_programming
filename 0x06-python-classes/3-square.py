8a02faf3cdcb808a3898e537a3bf4e81eb5a4c27
Maysummer <udojyk@gmail.com>
2022-06-23 20:36:32 -0700

    class to define square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+"""define a square"""
+
+
+class Square:
+    """defines a square with optional attribute
+    Attribute:
+        size(int): square size.
+    """
+    def __init__(self, size=0):
+        """instantiate the data"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be an integer")
+        self.__size = size
+    def area(self):
+        """returns current square area"""
+        return self.__size ** 2

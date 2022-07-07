383a412a1a7bf02dfe8295eb65dc9f76badd4e99
Maysummer <udojyk@gmail.com>
2022-07-07 10:55:42 -0700

    read file with "with" and print to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""function that reads a text file (UTF8) and prints it to stdout:
+    """
 def read_file(filename=""):
+    """reafd file"""
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end='')

fde095e38b16d0ba1fc1052604db6cf4bebfd118
Maysummer <udojyk@gmail.com>
2022-07-07 05:53:24 -0700

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """an empty class BaseGeometry"""
+    def area(self):
+        """pub instance method"""
+        raise Exception('area() is not implemented')

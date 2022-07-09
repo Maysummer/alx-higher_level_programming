cfba24c40e01a0a1a3de48141099415530544d09
Maysummer <udojyk@gmail.com>
2022-07-09 07:39:51 -0700

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class Student that defines a student by"""
+
+
+class Student:
+    """class Student that defines a student by"""
+    def __init__(self, first_name, last_name, age):
+        """initialize object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        new_list = {}
+        if attrs is None:
+            return self.__dict__
+
+        for elem in attrs:
+            if elem in self.__dict__:
+                new_list[elem] = self.__dict__[elem]
+        return new_list

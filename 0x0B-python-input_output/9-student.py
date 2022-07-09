b177e517b79162bdfe2750b4a01ec4674101f5d6
Maysummer <udojyk@gmail.com>
2022-07-09 06:04:22 -0700

    student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,9 +6,9 @@ class Student:
     """ class Student that defines a student"""
     def __init__(self, first_name, last_name, age):
         """initialize object"""
-        first_name.self = first_name
-        last_name.self = last_name
-        age.self = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """method to that retrieves a dictionary representation

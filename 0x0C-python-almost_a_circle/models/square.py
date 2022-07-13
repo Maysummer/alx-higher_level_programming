09f260d9836d72ccfa0cf3f2b956c5bb8410f111
Maysummer <udojyk@gmail.com>
2022-07-12 18:01:42 -0700

    square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,8 +6,12 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-      """square class"""
-      def __init__(self, size, x=0, y=0, id=None):
-          """instantiate object"""
-          self.size = size
-          super().__init__(id, x, y, width, height)
+    """square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """instantiate object"""
+        self.size = size
+        super().__init__(id=id, width=size, height=size, x=x, y=y)
+
+    def __str__(self):
+        """overload str method"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)

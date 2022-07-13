a35da82193598ab588702ee689fe6a45e1ea1861
Maysummer <udojyk@gmail.com>
2022-07-12 19:42:26 -0700

    returns the list of the JSON string representation json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,3 +35,11 @@ class Base:
         filename = cls.__name__ + ".json"
         with open(filename, 'w') as f:
             f.write(my_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or len(json_string) == 0:
+            return "[]"
+        else:
+            return json.loads(json_string)

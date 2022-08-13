7703ccd9e1f7e1a4ed6b1c2298721c4e56c82f6f
Maysummer <udojyk@gmail.com>
2022-08-13 04:24:37 -0700

    inner join

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server
-SELECT tv_shows.title, tv_shows_genres.genre_id FROM tv_shows INNER JOIN tv_shows_genres ON tv_show.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_shows_genres.genre_id FROM tv_shows INNER JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;

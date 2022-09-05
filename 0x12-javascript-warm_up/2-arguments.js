36b6e9b0d8699798b5a7411d46900411a0e8c867
Maysummer <udojyk@gmail.com>
2022-09-05 09:33:38 -0700

    cmd line arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let args =  process.argv;
+if (args.length == 2)
+{
+	console.log('No argument');
+}
+else if (process.argv.length == 3)
+{
+	console.log('Argument found');
+}
+else
+{
+	console.log('Arguments found');
+}

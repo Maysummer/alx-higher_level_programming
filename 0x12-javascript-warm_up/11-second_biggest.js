9934a648e356005b4bbf74ffc231a6df451bdca5
Maysummer <udojyk@gmail.com>
2022-09-05 16:45:26 -0700

    update const object

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ const args = process.argv;
 if (args.length === 2 || args.length === 3) {
   console.log(0);
 } else {
-  const arr = args.slice(2, args.length - 1).sort();
+  const arr = args.slice(2).sort();
   console.log(arr[arr.length - 2]);
 }

cf77f518eb188a41c9df6348686d103669fa7dc9
Maysummer <udojyk@gmail.com>
2022-09-07 06:57:10 -0700

    second largest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,24 @@
 #!/usr/bin/node
-const args = process.argv;
-if (args.length === 2 || args.length === 3) {
+const arg = process.argv;
+if (arg.length === 2 || arg.length === 3) {
   console.log(0);
 } else {
-  const arr = args.slice(2).sort();
-  console.log(arr[arr.length - 2]);
+  const args = arg.slice(2);
+  const newArr = [];
+  const max = Math.max(...args);
+  let secondLargest;
+  for (const i in args) {
+    newArr.push(max - args[i]);
+    for (const j in newArr) {
+      if (newArr[j] === 0) {
+        newArr[j] = max;
+      }
+      const min = Math.min(...newArr);
+      if (newArr[j] === min) {
+        const secondIndex = j;
+        secondLargest = args[secondIndex];
+      }
+    }
+  }
+  console.log(secondLargest);
 }

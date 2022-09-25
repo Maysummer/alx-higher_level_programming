5ec81d924587b58331b305d7fefa7a5e96de2351
Maysummer <udojyk@gmail.com>
2022-09-25 16:43:32 -0700

    status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script to fetch a resource"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body reaponse:)
+        print('\t- type: {}'.format(type(html)));
+        print('\t-  content: {}'.format(html))
+        print('\t utf8 content: {}'.format(html.decode("utf-o8"))

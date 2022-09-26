3da8fe922706e59daaf811a4803be811064a90a4
Maysummer <udojyk@gmail.com>
2022-09-25 23:52:00 -0700

    edit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
-        print('Body reaponse:)
+        print('Body reaponse:')
         print('\t- type: {}'.format(type(html)));
         print('\t-  content: {}'.format(html))
-        print('\t utf8 content: {}'.format(html.decode("utf-o8"))
+        print('\t utf8 content: {}'.format(html.decode("utf-8"))
